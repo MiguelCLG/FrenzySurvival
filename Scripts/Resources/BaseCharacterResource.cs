92069ca3c7eda8a57ffd755bead5b207c678c541
Lavizord <63145101+Lavizord@users.noreply.github.com>
2024-11-02 12:34:55 +0000

    fix: por healthbar hidden, linked to flag in mobResource

--- Scripts/Resources/BaseCharacterResource.cs
+++ Scripts/Resources/BaseCharacterResource.cs
@@ -12,6 +12,7 @@ public partial class BaseCharacterResource : Resource
   [Export] public int KI;
   [Export] public int MaxKI;
   [Export] public SpriteFrames AnimatedFrames;
+  [Export] public bool ShowHealBar = true;
   [Export] public Texture2D Portrait;
   [Export] public Array<AbilityResource> Abilities;
   [Export] public Array<LootTable> LootTables;
